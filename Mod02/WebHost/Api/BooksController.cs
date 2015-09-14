7a474c02c4c531174e25f85168c72a8f5559d315
Maurice de Beijer <mauricedb@computer.org>
2015-09-14 13:56:00 +0100

    HTTP Methods and StatusCodes

---
+++ Mod02/WebHost/Api/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using RestDemo.Models;
+
+namespace WebHost.Api
+{
+    public class BooksController : ApiController
+    {
+        private IBooksRepository _repo = new BooksRepository();
+
+        public Task<IEnumerable<Book>> Get()
+        {
+            return _repo.GetBooks();
+        }
+
+        public async Task<HttpResponseMessage> Get(int id)
+        {
+            var book = await _repo.GetBook(id);
+
+            if (book == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, book);
+        }
+    }
+}
