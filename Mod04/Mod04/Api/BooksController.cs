14d38214df89bef441be11bfb8e8764eff91bca5
Maurice de Beijer <mauricedb@computer.org>
2015-09-15 10:23:23 +0100

    Browser based clients

---
+++ Mod04/Mod04/Api/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Mod04.Models;
+
+namespace Mod04.Api
+{
+    public class BooksController : ApiController
+    {
+        private readonly IBooksRepository _repo = new BooksRepository();
+
+        // GET api/books
+        public Task<IEnumerable<Book>> Get()
+        {
+            return _repo.GetBooks();
+        }
+
+        // GET api/books/5
+        public async Task<HttpResponseMessage> Get(int id)
+        {
+            var book = await _repo.GetBook(id);
+            if (book == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            book.Title = DateTime.Now.ToLongTimeString();
+            var response = Request.CreateResponse(HttpStatusCode.OK, book);
+
+            //response.Headers.CacheControl = new CacheControlHeaderValue();
+
+            return response;
+        }
+    }
+}
---
+++ Mod04/Mod04/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Mod04.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
